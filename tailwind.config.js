4a03043fa6af07554fbc8a984755b26606a1f2b9
Luca Di Marco <lucanahtsu@gmail.com>
2023-10-06 14:20:52 -0300

    Add white color and background images

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,15 @@ module.exports = {
       'darkcyan': '	#19a2ae',
       'verydarkdesaturatedblue': '#2d3248',
       'darkgrayishblue': '#6a6f81',
-      'darkgray': '#969696'
+      'darkgray': '#969696',
+      'white': '#ffff'
     },
     fontFamily: {
       'kumbhsans': ['Kumbh Sans', 'sans-serif']
+    },
+    backgroundImage: {
+      'patterntop': "url('./images/bg-pattern-top.svg')",
+      'patternbot': "url('./images/bg-pattern-bottom.svg')"
     },
     extend: {},
   },
