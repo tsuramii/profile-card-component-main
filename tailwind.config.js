6f496a26d354a743e6c61648453ca971041fa579
Luca Di Marco <lucanahtsu@gmail.com>
2023-10-06 11:58:15 -0300

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    colors:{
+      'darkcyan': '	#19a2ae',
+      'verydarkdesaturatedblue': '#2d3248',
+      'darkgrayishblue': '#6a6f81',
+      'darkgray': '#969696'
+    },
+    fontFamily: {
+      'kumbhsans': ['Kumbh Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
